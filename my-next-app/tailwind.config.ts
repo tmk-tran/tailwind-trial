cfd118d4715d2d3057551d2a55e1659f1d9ca47e
T Mark <tmk1.tran@gmail.com>
2024-07-23 19:18:13 -0500

    successful target of tailwind colors to match the figma design, set background color

--- my-next-app/tailwind.config.ts
+++ my-next-app/tailwind.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        background: "hsl(224 71.4% 4.1%)", // Sets black color of headers, sidebar, and card sections
+        primary: "hsl(208 100 29)", // Sets the background color behind cards and header, also the light blue at bottom of cards
+        // foreground: "hsl(224, 71.4%, 4.1%)", // Unused atm
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
